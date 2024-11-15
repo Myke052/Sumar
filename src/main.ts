750f16adce9e029ec21af5de78bcb0746b0e97bb
Maycol Galarza <cuentacorreos200@gmail.com>
2024-11-14 23:02:45 -0500

    intentando arreglar puerto

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Configura CORS para permitir acceso público
+  app.enableCors({
+    origin: '*', // Permite todas las solicitudes desde cualquier origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
+    credentials: false, // No permite el uso de cookies
+  });
+
+  // Configuración de archivos estáticos y tuberías de validación
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  await app.listen(process.env.PORT ?? 3000);
   app.useGlobalPipes(new ValidationPipe());
+
+  // Iniciar la aplicación en el puerto de Vercel o en el 3000 localmente
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
